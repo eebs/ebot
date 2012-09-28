9be1eb84893a1833313af0704007583110d15f66
Ebrahim Kobeissi <eebs@eebs-tech.com>
2012-09-27 23:24:10 -0400

    added tf2 leaf

---
+++ leaves/tf2/controller.rb
@@ -0,0 +1,105 @@
+# Controller for the Tf2 leaf.
+
+require 'open-uri'
+class Controller < Autumn::Leaf
+  before_filter :base_url
+
+  # Typing "!about" displays some basic information about this leaf.
+
+  def about_command(stem, sender, reply_to, msg)
+    # This method renders the file "about.txt.erb"
+  end
+
+  def tf2_command(stem, sender, reply_to, msg)
+    url = @base_url + 'serverinfo/' + @server_ip
+    doc = Nokogiri::XML(open(url))
+    server = doc>'gameME'>'serverinfo'>'server'
+    map = (server>'map').text
+    act = (server>'act').text
+    max = (server>'max').text
+    map + ': ' + act + '/' + max
+  end
+
+  def players_command(stem, sender, reply_to, msg)
+    url = @base_url + 'serverinfo/' + @server_ip + '/players'
+    doc = Nokogiri::XML(open(url))
+    playersXML = doc>'gameME'>'serverinfo'>'server'>'players'>'player'
+    playerCount = playersXML.size
+    players = Array.new
+    playersXML.each do |p|
+        name = (p>'name').text
+        kills = (p>'kills').text
+        deaths = (p>'deaths').text
+        players << name + '[' + kills + '/' + deaths + ']'
+    end
+    'Players: ' + playerCount.to_s + "\n " + players.join(', ')
+  end
+
+  def huggle_command(stem, sender, reply_to, msg)
+    '/me huggles ' + sender[:nick]
+  end
+
+  def awards_command(stem, sender, reply_to, msg)
+    # Clear the cached player lsit
+    @playerlist = nil
+    name = msg
+    # Use the sender's name if they don't provide one
+    name ||= sender[:nick]
+    output = ''
+    count = get_players_by_name_count(name)
+    players = get_players_by_name(name)
+    return "Can't find '" + name + "'." unless count.to_i > 0
+    player = players.first
+    playerName = (player>'name').text
+    if(count.to_i > 1) then
+        output += count + " players matching '" + name + "'\n"
+        output += "Listing award for '" + playerName + "', be more specific if this isn't you.\n"
+    end
+    output += get_awards_output(player)
+  end
+
+  protected
+
+  def get_awards_output(player)
+    name = (player>'name').text
+    steamID = (player>'uniqueid').text
+    url = @base_url + 'playerinfo/tf/' + steamID + '/awards'
+    doc = Nokogiri::XML(open(url))
+    player = doc>'gameME'>'playerinfo'>'player'
+    awards = player>'awards'>'award'
+    todaysAwards = Array.new
+    awards.each do |a|
+        awardDate = DateTime.parse((a>'date').text)
+        if(awardDate == DateTime.yesterday) then
+            name = (a>'name').text
+            count = (a>'count').text
+            todaysAwards << name + ' ( ' + count + ' )'
+        end
+    end
+    return "No awards for " + name + "." if todaysAwards.empty?
+    todaysAwards.join("\n")
+  end
+
+  def get_players_by_name(name)
+    @playerlist ||= get_playerlist(name)
+    @playerlist>'player'
+  end
+
+  def get_players_by_name_count(name)
+    @playerlist ||= get_playerlist(name)
+    count = @playerlist>'pagination'>'totalcount'
+    count.text
+  end
+
+  def get_playerlist(name)
+    name = URI.escape(name)
+    url = @base_url + 'playerlist/tf/name/' + name
+    doc = doc = Nokogiri::XML(open(url))
+    doc>'gameME'>'playerlist'
+  end
+
+  def base_url_filter(stem, channel, sender, command, msg, opts)
+    @base_url ||= 'http://stats4.artofwarcentral.com/api/'
+    @server_ip ||= '75.102.47.102:27015'
+  end
+end
