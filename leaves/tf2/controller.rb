9d6b0c901852c5c6f3bc948c169ccbc61aa5890f
Ebrahim Kobeissi <eebs@eebs-tech.com>
2013-01-30 18:24:19 +0000

    Updated stats site url

--- leaves/tf2/controller.rb
+++ leaves/tf2/controller.rb
@@ -113,7 +113,7 @@ class Controller < Autumn::Leaf
   end
 
   def base_url_filter(stem, channel, sender, command, msg, opts)
-    @base_url ||= 'http://teamfuncom.gameme.com/api/'
+    @base_url ||= 'http://teamfuncom2.gameme.com/api/'
     @server_ip ||= '75.102.47.102:27015'
   end
 end
