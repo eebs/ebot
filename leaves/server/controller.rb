2cacc2f8154aa207be6089a1fbf809c0d49083d7
Ebrahim Kobeissi <eebs@eebs-tech.com>
2013-06-10 14:59:05 -0400

    added server leaf

---
+++ leaves/server/controller.rb
@@ -0,0 +1,16 @@
+# Controller for the Server leaf.
+
+class Controller < Autumn::Leaf
+  
+  # Typing "!about" displays some basic information about this leaf.
+  
+  def about_command(stem, sender, reply_to, msg)
+    # This method renders the file "about.txt.erb"
+  end
+
+  def update(stem, sender, reply_to, msg)
+    stem.message 'Updating TF2 server'
+    `cd /home/tf2server && ./steamcmd.sh +runscript update_tf2.txt`
+    stem.message 'Finished updating TF2 server'
+  end
+end
