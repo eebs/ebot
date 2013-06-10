881e0561ba9dd35c86d8a2f1f906504b92661d29
Ebrahim Kobeissi <eebs@eebs-tech.com>
2013-06-10 15:22:56 -0400

    added _command

--- leaves/server/controller.rb
+++ leaves/server/controller.rb
@@ -8,7 +8,7 @@ class Controller < Autumn::Leaf
     # This method renders the file "about.txt.erb"
   end
 
-  def update(stem, sender, reply_to, msg)
+  def update_command(stem, sender, reply_to, msg)
     stem.message 'Updating TF2 server'
     `cd /home/tf2server && ./steamcmd.sh +runscript update_tf2.txt`
     stem.message 'Finished updating TF2 server'
